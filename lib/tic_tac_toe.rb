e177089eeb0436fffa49a8b9341f8ed89af2c0d9
Samantha Jordan <shinyguns@gmail.com>
2020-02-10 22:35:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,14 @@ class TicTacToe
   ]
   
   #display_board
+    def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
   
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
@@ -26,14 +34,7 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
  
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-  
+
 
   
   def input_to_index(input)
